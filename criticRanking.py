8b13a0743a3c72a840c83a6c497b74b86c4d59d9
Dadoright <donabusi1@gmail.com>
2021-01-14 11:47:35 +0530

    a new python file is added so it will give most corealated critic to a inserted person

--- criticRanking.py
+++ criticRanking.py
@@ -1,5 +1,4 @@
-import math
-
+import pearsonCorealation as PC
 #this is a Dictionary of movie critics
 
 critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
@@ -20,35 +19,11 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
  'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
 
-#to return pearson corealation for p1 and p2
+#a function to return the best matches
 
-def pearson(prefs,p1,p2):
-    si={}
-    for item in prefs[p1]:
-        if item in prefs[p2]:
-            si[item]=1
+def topMatch(prefs,p1,n=5,similarity=PC.pearson):
+    score = [(similarity(prefs,p1,other),other) for other in prefs if other!=p1]
 
-    #number of elements in the critics dictionary (eg Lisa Rose....Toby)
-    n=len(si)
-
-    if n==0:
-        return 0
-
-    #adding the scores together
-    sum1 = sum(prefs[p1][item] for item in si)
-    sum2 = sum(prefs[p2][item] for item in si)
-
-    #adding squres of scores together
-    sum1sq = sum([pow(prefs[p1][item],2)for item in si])
-    sum2sq = sum([pow(prefs[p2][item],2)for item in si])
-
-    #getiing the product of the score
-    psum = sum([prefs[p1][item]*prefs[p2][item] for item in si])
-
-    #getting the pearson score
-    num = psum-(sum1*sum2/n)
-    den = math.sqrt((sum1sq-pow(sum1,2)/n)*(sum2sq-pow(sum2,2)/n))
-    if den ==0:
-        return 0
-    r = num/den
-    return r
+    score.sort()
+    score.reverse()
+    return score[0:n]
