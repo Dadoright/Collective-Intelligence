a44a49753401ccc611c5155ef59542df37266b82
Dadoright <donabusi1@gmail.com>
2021-01-16 13:25:44 +0530

    the lessons are too advanced to be carried on

--- generateFeedVector.py
+++ generateFeedVector.py
@@ -24,3 +24,35 @@ def getwords(html):
     words=re.compile(r'[^A-Z^a-z]+').split(txt)
 
     return[word.lower() for word in words if word!='']
+
+apcount={}
+wordcounts={}
+f = open("feedlist.txt","r")
+for feedurl in f:
+    title,wc = getwordcount(feedurl)
+    wordcounts[title] = wc
+    for word,count in wc.items():
+        apcount.setdefault(word,0)
+        if count>1:
+            apcount[word]+=1
+
+    wordlist=[]
+    for w,bc in apcount.items():
+        frac=float(bc)/len(feeslist)
+        if frac>0.1 and frac<0.5:
+            wordlist.append(w)
+
+    
+    out = open("blogdata.txt","a")
+    out.write('blog')
+    for word in wordlist:
+        out.write('\t%s' %word)
+    out.write('\n')
+    for blog,wc in wordcounts.items():
+        out.write(blog)
+        for word in wordlist:
+            if word in wc:
+                out.write('\t%d' %wc[word])
+            else:
+                out.write('\t0')
+        out.write('\n')
